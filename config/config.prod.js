856d6f8781cd21f2bf434f86997650c16075fc23
liangshaojie <877942720@qq.com>
2019-12-25 17:34:10 +0800

    修改生产环境的配置

--- config/config.prod.js
+++ config/config.prod.js
@@ -4,9 +4,34 @@ const path = require('path')
 module.exports = appInfo => {
 
     return {
-        // 插件路径
-        admin_root_path: 'https://cdn.html-js.cn/cms/plugins/static/admin',
-        // 数据库连接
+        admin_root_path: 'http://localhost',
+        // DEV_CONFIG_MODULES_BEGIN
+        dev_modules: [
+            // 'navbar',
+            // 'dashboard',
+            // 'adminGroup',
+            // 'adminUser',
+            // 'adminResource',
+            // 'systemConfig',
+            // 'backUpData',
+            // 'systemOptionLog',
+            // 'announce',
+            // 'systemNotify',
+            // 'ads',
+            // 'contentTemp',
+            // 'templateConfig',
+            // 'versionManage',
+            // 'content',
+            // 'contentTags',
+            // 'contentCategory',
+            // 'contentMessage',
+            // 'regUser',
+            // 'helpCenter',
+            // 'renderCms',
+            // 'cmsTemplate',
+            // 'plugin'
+        ],
+        // DEV_CONFIG_MODULES_END
         mongoose: {
             client: {
                 url: 'mongodb://127.0.0.1:27017/doracms2',
@@ -16,21 +41,36 @@ module.exports = appInfo => {
                 },
             },
         },
-        // 静态目录
+        mysql: {
+            // 单数据库信息配置
+            client: {
+                // host
+                host: '192.168.3.250',
+                // 端口号
+                port: '33306',
+                // 用户名
+                user: 'root',
+                // 密码
+                password: 'root',
+                // 数据库名
+                database: 'user',
+            },
+            // 是否加载到 app 上，默认开启
+            app: true,
+            // 是否加载到 agent 上，默认关闭
+            agent: false,
+        },
         static: {
             prefix: '/static',
-            dir: [path.join(appInfo.baseDir, 'app/public'), '/home/doraData/uploadFiles/static'],
+            dir: [path.join(appInfo.baseDir, 'app/public'), path.join(appInfo.baseDir, 'backstage/dist')],
             maxAge: 31536000,
         },
-        // 日志路径
         logger: {
-            dir: '/home/doraData/logsdir/doracms',
+            dir: path.join(appInfo.baseDir, 'logs'),
         },
-        // 文件上传路径
-        upload_path: '/home/doraData/uploadFiles/static',
-        // 服务地址配置
-        server_path: 'https://www.html-js.cn',
-        server_api: 'https://www.html-js.cn/api',
+        upload_path: process.cwd() + '/app/public', // 文件上传路径(为了跟ueditor统一，不加upload))
+        server_path: 'http://localhost:9090',
+        server_api: 'http://localhost:9090/api',
 
     }
 };
